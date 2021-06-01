899a92e5c9a4fac10848a953daa59a9937580391
MakarijeSpasic <makarije.spasic@gmail.com>
2021-06-01 23:46:29 +0200

    Ajout de conditions dans les méthodes de gestion de caractéristiques

--- src/PersonnageJoueur.cpp
+++ src/PersonnageJoueur.cpp
@@ -10,41 +10,61 @@ PersonnageJoueur::PersonnageJoueur(int initialCD, int initialVD,
 , Portee{initialPortee}
 , Degats{initialDegats}
 {}
-
+int limitTerrainX;
+int limitTerrainY;
 void PersonnageJoueur::Deplacer(std::string direction) {
 	if (direction == "Gauche") {
-		this->coord.x = this->coord.x - 1;
+		if (this->coord.x > 0) {
+			this->coord.x = this->coord.x - 1;
+		}
 	}
 	else if (direction == "Droite") {
-		this->coord.x = this->coord.x + 1;
+		if (this->coord.x < limitTerrainX) {
+			this->coord.x = this->coord.x + 1;
+		}
 	}
 	else if (direction == "Haut") {
-		this->coord.y = this->coord.y + 1;
+		if (this->coord.y < limitTerrainY) {
+			this->coord.y = this->coord.y + 1;
+		}
 	}
 	else if (direction == "Bas"){
-		this->coord.y = this->coord.y - 1;
+		if (this->coord.y > 0) {
+			this->coord.y = this->coord.y - 1;
+		}
 	}
 };
 void PersonnageJoueur::Apply_damage(int damage) {
 	
 };
 void PersonnageJoueur::Lose_range(std::string typeObstacle) {
-	if (typeObstacle == "Gauche") {
-		this->coord.x = this->coord.x - 1;
+	if (typeObstacle == "Monstre1") {
+		this->Portee = this->Portee - 1;
 	}
-	else if (typeObstacle == "Droite") {
-		this->coord.x = this->coord.x + 1;
+	else if (typeObstacle == "Monstre2") {
+		this->Portee = this->Portee - 2;
 	}
-	else if (typeObstacle == "Haut") {
-		this->coord.y = this->coord.y + 1;
+	else if (typeObstacle == "Monstre3") {
+		this->Portee = this->Portee - 3;
 	}
-	else if (typeObstacle == "Bas") {
-		this->coord.y = this->coord.y - 1;
+	else if (typeObstacle == "Mur") {
+		this->Portee = this-> Portee - 1;
 	}
 };
-void Lose_damage(std::string typeObstacle) {
-
-};
-void Hit() {
+void PersonnageJoueur::Lose_damage(std::string typeObstacle) {
+	if (typeObstacle == "Monstre1") {
+		this->Degats = this->Degats - 1;
+	}
+	else if (typeObstacle == "Monstre2") {
+		this->Degats = this->Degats - 2;
+	}
+	else if (typeObstacle == "Monstre3") {
+		this->Degats = this->Degats - 3;
+	}
+	else if (typeObstacle == "Mur") {
+		this->Degats = this->Degats - 1;
+	};
+}
+void PersonnageJoueur::Hit() {
 
 };
