624a0cddd836fcc9a023bef7886ef5ef31b9d885
Eliade <raph.gnlzs@gmail.com>
2021-06-30 19:53:19 +0200

    chaque monstre a une forme différente

--- src/Monstre.cpp
+++ src/Monstre.cpp
@@ -5,7 +5,17 @@ Monstre::Monstre(b2World* world, int zoneActivation, TypeMonstre type, float wrl
 	, zoneActivation(zoneActivation)
 	, type(type)
 {
-	shape = sf::CircleShape(convertCoord_fromWorld_toWindow(b2Vec2(1.f, 1.f)).x);//On converti la largeur du rectangle en rayon du cercle
+	switch (type) {
+	case Monstre1:
+		shape = sf::CircleShape(convertCoord_fromWorld_toWindow(b2Vec2(1.f, 1.f)).x, 3); //C'est un triangle
+		break;
+	case Monstre2:
+		shape = sf::CircleShape(convertCoord_fromWorld_toWindow(b2Vec2(1.f, 1.f)).x, 4); //C'est un pentagone
+		break;
+	case Monstre3:
+		shape = sf::CircleShape(convertCoord_fromWorld_toWindow(b2Vec2(1.f, 1.f)).x,5); //C'est un octogone
+	}
+	//au dessus : On converti la largeur du rectangle(la boite qui sert de body à l'entite) en rayon du cercle pour que ce soit cohérent
 	shape.setFillColor(sf::Color::Red); //On met la couleur rouge pour différencier du joueur
 	shape.setPosition(convertCoord_fromWorld_toWindow(body->GetTransform().p));
 	isPlayer = false;
